93768c31ade37714979d409a4f57bb3ee93605e1
Mabast <mabast.niga@yahoo.com>
2022-05-18 00:18:44 -0500

    Created components and connected to index.jsx

--- ecommerce/pages/index.js
+++ ecommerce/pages/index.js
@@ -1,15 +1,19 @@
 import React from "react";
 
+import { Product, FooterBanner, HeroBanner } from "../components";
+
 const Home = () => {
   return (
     <>
-      HeroBanner
-      <div>
+      <HeroBanner />
+      <div className="products-heading">
         <h2>Best Selling Products</h2>
         <p>Speakers of many variations</p>
       </div>
-      <div>{["product 1", "product 2"].map((product) => product)}</div>
-      Footer
+      <div className="products-container">
+        {["product 1", "product 2"].map((product) => product)}
+      </div>
+      <FooterBanner />
     </>
   );
 };
