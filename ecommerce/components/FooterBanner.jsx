927f0c5d7227439077e042eb27becfda9195e01c
Mabast <mabast.niga@yahoo.com>
2022-05-18 14:51:43 -0500

    created single product page

--- ecommerce/components/FooterBanner.jsx
+++ ecommerce/components/FooterBanner.jsx
@@ -22,7 +22,6 @@ const FooterBanner = ({
       <div className="banner-desc">
         <div className="left">
           <p>{discount}</p>
-
           <h3>{largeText1}</h3>
           <h3>{largeText2}</h3>
           <p>{saleTime}</p>
