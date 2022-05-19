5bda15c611f837f98bb2cff4b2e8cebc3ba774e1
Mabast <mabast.niga@yahoo.com>
2022-05-19 16:59:51 -0500

    created states

--- ecommerce/pages/product/[slug].js
+++ ecommerce/pages/product/[slug].js
@@ -9,6 +9,7 @@ import {
 import { client, urlFor } from "../../lib/client";
 
 import { Product } from "../../components";
+import { useStateContext } from "../../context";
 
 const productDetails = ({ product, products }) => {
   const { image, name, details, price } = product;
