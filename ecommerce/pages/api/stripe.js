7fd84813b0a9e681b34f35ba4572a4df56d79468
Mabast <mabast.niga@yahoo.com>
2022-05-20 02:11:14 -0500

    added Stripe API connections

--- ecommerce/pages/api/stripe.js
+++ ecommerce/pages/api/stripe.js
@@ -1,3 +1,39 @@
-import Stripe from "stripe";
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 
-const stripe = new Stripe();
+export default async function handler(req, res) {
+  if (req.method === "POST") {
+    console.log(req.body.cartItems);
+    try {
+      // Create Checkout Sessions from body params.
+      const params = {
+        submit_type: "pay",
+        mode: "payment",
+        payment_method_types: ["card"],
+        billing_address_collection: "auto",
+        shipping_options: [
+          { shipping_rate: "shr_1L1PKWKrg2XulFtLSvfCrpQc" },
+          { shipping_rate: "shr_1L1PLRKrg2XulFtLbZv3LXdJ" },
+        ],
+
+        line_items: [
+          {
+            // Provide the exact Price ID (for example, pr_1234) of the product you want to sell
+            price: "{{PRICE_ID}}",
+            quantity: 1,
+          },
+        ],
+        mode: "payment",
+        success_url: `${req.headers.origin}/?success=true`,
+        cancel_url: `${req.headers.origin}/?canceled=true`,
+      };
+
+      const session = await stripe.checkout.sessions.create();
+      res.redirect(303, session.url);
+    } catch (err) {
+      res.status(err.statusCode || 500).json(err.message);
+    }
+  } else {
+    res.setHeader("Allow", "POST");
+    res.status(405).end("Method Not Allowed");
+  }
+}
