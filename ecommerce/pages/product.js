260889c71d227a772e6f0a90d1df68df8d895733
Mabast <mabast.niga@yahoo.com>
2022-05-22 00:46:29 -0500

    fixed: shop now button not working

---
+++ ecommerce/pages/product.js
@@ -0,0 +1,31 @@
+import React from "react";
+
+import { client } from "../lib/client";
+import { Product } from "../components";
+
+const Home = ({ products }) => {
+  return (
+    <div>
+      <div className="products-heading">
+        <h2>Best Selling Products</h2>
+        <p>Speakers of many variations</p>
+      </div>
+      <div className="products-container">
+        {products?.map((product) => (
+          <Product key={product._id} product={product} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export const getServerSideProps = async () => {
+  const query = '*[_type == "product"]';
+  const products = await client.fetch(query);
+
+  return {
+    props: { products },
+  };
+};
+
+export default Home;
